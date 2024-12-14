fbaf173c26068e98de1e1f0bf691464578d72f11
Kévin Le Perf <codlabtech@gmail.com>
2024-12-15 00:25:11 +0100

    add js

---
+++ jsApp/build.gradle.kts
@@ -0,0 +1,31 @@
+plugins {
+    alias(additionals.plugins.kotlin.multiplatform)
+    alias(additionals.plugins.jetbrains.compose)
+    alias(additionals.plugins.compose.compiler)
+}
+
+kotlin {
+    js(IR) {
+        browser()
+        binaries.executable()
+    }
+
+    sourceSets {
+        val jsMain by getting {
+            dependencies {
+                implementation(project(":shared"))
+                implementation(compose.runtime)
+                implementation(compose.html.core)
+                implementation(compose.ui)
+                implementation(compose.material)
+                implementation(libs.skiko)
+                api(additionals.multiplatform.precompose.web)
+                api(additionals.multiplatform.safearea)
+            }
+        }
+    }
+}
+
+compose.experimental {
+    web.application {}
+}
---
+++ jsApp/src/jsMain/kotlin/Main.kt
@@ -0,0 +1,20 @@
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.ui.Modifier
+import eu.codlab.lorcana.blipya.home.App
+import moe.tlaster.precompose.preComposeWindow
+import org.jetbrains.skiko.wasm.onWasmReady
+
+@OptIn(ExperimentalJsExport::class)
+@JsExport
+fun main() {
+    onWasmReady {
+        preComposeWindow(
+            title = "Blipya's Lorcana Tool"
+        ) {
+            Column(modifier = Modifier.fillMaxSize()) {
+                App(false)
+            }
+        }
+    }
+}
