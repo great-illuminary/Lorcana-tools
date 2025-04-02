6c84b4bb80dfbbfc1a7847880ad4fba3d02e215c
Kevin Le Perf <codlabtech@gmail.com>
2025-04-02 19:01:20 +0200

    Add origin of the exception back to the updated Sentry lib

--- shared/src/commonMain/kotlin/eu/codlab/lorcana/blipya/utils/ViewModel.kt
+++ shared/src/commonMain/kotlin/eu/codlab/lorcana/blipya/utils/ViewModel.kt
@@ -3,11 +3,24 @@ package eu.codlab.lorcana.blipya.utils
 import dev.icerock.moko.mvvm.viewmodel.ViewModel
 import eu.codlab.sentry.wrapper.Sentry
 import eu.codlab.viewmodel.launch
+import kotlinx.coroutines.CancellationException
 import kotlinx.coroutines.CoroutineScope
 
 fun ViewModel.safeLaunch(
     onError: (Throwable) -> Unit = {
-        Sentry.captureException(it)
+        if(it !is CancellationException) {
+            Sentry.captureException(it)
+        }
     },
     run: suspend CoroutineScope.() -> Unit
-) = launch(onError, run)
+) = OriginalException().let { original ->
+    launch(
+        onError = { error ->
+            error.addSuppressed(original)
+            onError(error)
+        },
+        run
+    )
+}
+
+class OriginalException : Exception()
